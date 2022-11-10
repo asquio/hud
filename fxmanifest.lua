bcbbdc0aa07b04fcca09adf3f2a1d2633c057a8f
asquio <117231218+asquio@users.noreply.github.com>
2022-11-10 10:01:56 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+lua54 "yes"
+game "gta5"
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/css/grid.css',
+	'html/js/main.js',
+	'html/img/*',
+	'html/fonts/*'
+}
+
+server_script 'server.lua'
+
+client_script 'client.lua'
